78c9fabadd9ffca01f29a3d826ffed6554fb9813
Chinedu Anyanwu <colchinedu@gmail.com>
2025-08-19 22:34:08 -0600

    Added a step in basic cleaning to drop rows that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    # Drop rows in dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
 
     # Convert last_review to datetime
     logger.info("convert last_review to datetime")
